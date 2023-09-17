f177ba55ae95c5adf13139ce5979089714e78fae
Ruan Montelo <ruanmontelo@gmail.com>
2023-09-17 00:02:08 -0300

    Changed ambient, diffuse, clear and fragment shader colors to create a "factory lighting"

--- Assets/Shaders/1.light_cube.fs
+++ Assets/Shaders/1.light_cube.fs
@@ -4,5 +4,6 @@ out vec4 FragColor;
   
 void main()
 {
-	FragColor = vec4(1.0);
+    // Blue
+	FragColor = vec4(vec3(0.0, 0.0, 0.3), 1.0);
 }
--- Main.cpp
+++ Main.cpp
@@ -46,6 +46,11 @@ Shader* lightCubeShader;
 // Wireframe toggle
 bool wireframeToggle = false;
 
+// Point light intensity configuration
+const float constant = 1.0f;
+const float linear = 0.7f;
+const float quadratic = 1.8f;
+
 int main()
 {
 	// glfw: initialize and configure
@@ -218,9 +223,6 @@ int main()
 	// load the specular map image.
 	unsigned int specularMap = loadTexture("Assets\\Images\\container2_specular.png");
 
-	// Load the emission map image.
-	// unsigned int emissionMap = loadTexture("Assets\\Images\\matrix.jpg");
-
 	unsigned int lightCubeVAO;
 	glGenVertexArrays(1, &lightCubeVAO);
 	glBindVertexArray(lightCubeVAO);
@@ -242,11 +244,8 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.03f, 0.03f, 0.03f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// lightPos update
-		// glm::vec3 lightPosOrbit = glm::vec3(glm::sin(currentFrame * 2.0f) * lightPos.x, lightPos.y, glm::cos(currentFrame * 2.0f) * lightPos.z);
 
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader->use();
@@ -262,55 +261,52 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, specularMap);
 		
-		// The third one that emits light
-		//glActiveTexture(GL_TEXTURE2);
-		//glBindTexture(GL_TEXTURE_2D, emissionMap);
-
 		// Set the material
 		lightingShader->setInt("material.diffuse", 0);
 		lightingShader->setInt("material.specular", 1);
 		lightingShader->setInt("material.emission", 2);
 		lightingShader->setFloat("material.shininess", material.shininess);
 
-		lightingShader->setFloat("pointLights[0].constant", 1.0f);
-
 		// directional light
 		lightingShader->setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-		lightingShader->setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-		lightingShader->setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+		//lightingShader->setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+		//lightingShader->setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+
+		lightingShader->setVec3("dirLight.ambient", 0.01f, 0.01f, 0.2f);
+		lightingShader->setVec3("dirLight.diffuse", 0.01f, 0.01f, 0.2f);
 		lightingShader->setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
 		// point light 1
 		lightingShader->setVec3("pointLights[0].position", pointLightPositions[0]);
 		lightingShader->setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
 		lightingShader->setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
 		lightingShader->setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
-		lightingShader->setFloat("pointLights[0].constant", 1.0f);
-		lightingShader->setFloat("pointLights[0].linear", 0.09f);
-		lightingShader->setFloat("pointLights[0].quadratic", 0.032f);
+		lightingShader->setFloat("pointLights[0].constant", constant);
+		lightingShader->setFloat("pointLights[0].linear", linear);
+		lightingShader->setFloat("pointLights[0].quadratic", quadratic);
 		// point light 2
 		lightingShader->setVec3("pointLights[1].position", pointLightPositions[1]);
 		lightingShader->setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
 		lightingShader->setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
 		lightingShader->setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
-		lightingShader->setFloat("pointLights[1].constant", 1.0f);
-		lightingShader->setFloat("pointLights[1].linear", 0.09f);
-		lightingShader->setFloat("pointLights[1].quadratic", 0.032f);
+		lightingShader->setFloat("pointLights[1].constant", constant);
+		lightingShader->setFloat("pointLights[1].linear", linear);
+		lightingShader->setFloat("pointLights[1].quadratic", quadratic);
 		// point light 3
 		lightingShader->setVec3("pointLights[2].position", pointLightPositions[2]);
 		lightingShader->setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
 		lightingShader->setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
 		lightingShader->setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
-		lightingShader->setFloat("pointLights[2].constant", 1.0f);
-		lightingShader->setFloat("pointLights[2].linear", 0.09f);
-		lightingShader->setFloat("pointLights[2].quadratic", 0.032f);
+		lightingShader->setFloat("pointLights[2].constant", constant);
+		lightingShader->setFloat("pointLights[2].linear", linear);
+		lightingShader->setFloat("pointLights[2].quadratic", quadratic);
 		// point light 4
 		lightingShader->setVec3("pointLights[3].position", pointLightPositions[3]);
 		lightingShader->setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
 		lightingShader->setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
 		lightingShader->setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
-		lightingShader->setFloat("pointLights[3].constant", 1.0f);
-		lightingShader->setFloat("pointLights[3].linear", 0.09f);
-		lightingShader->setFloat("pointLights[3].quadratic", 0.032f);
+		lightingShader->setFloat("pointLights[3].constant", constant);
+		lightingShader->setFloat("pointLights[3].linear", linear);
+		lightingShader->setFloat("pointLights[3].quadratic", quadratic);
 		// spotLight
 		lightingShader->setVec3("spotLight.position", camera.Position);
 		lightingShader->setVec3("spotLight.direction", camera.Front);
@@ -320,8 +316,8 @@ int main()
 		lightingShader->setFloat("spotLight.constant", 1.0f);
 		lightingShader->setFloat("spotLight.linear", 0.09f);
 		lightingShader->setFloat("spotLight.quadratic", 0.032f);
-		lightingShader->setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-		lightingShader->setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
+		lightingShader->setFloat("spotLight.cutOff", glm::cos(glm::radians(7.5f)));
+		lightingShader->setFloat("spotLight.outerCutOff", glm::cos(glm::radians(12.5f)));
 
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
